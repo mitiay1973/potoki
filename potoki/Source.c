c8cb8057a8c0813791e059d1ce2f9eeb4e5784b6
Dima Sorokin <sorokindima36@gmail.com>
2022-11-23 09:30:06 +0300

    Добавьте файлы проекта.

---
+++ potoki/Source.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <Windows.h>
+#include < time.h >
+int timers(long int t)
+{
+	int sec = t;
+	int min = 0;
+	int hours = 0;
+	while (1)
+	{
+		while (sec > 60)
+		{
+			sec = sec - 60;
+			min++;
+		}
+		while (min > 60)
+		{
+			min = min - 60;
+			hours++;
+		}
+		while (hours > 24)
+		{
+			hours = hours - 24;
+			if (hours < 24)
+			{
+				hours = hours + 3;
+			}
+		}
+		if (hours == 0 && min == 0 && sec == 0)
+		{
+			printf_s("Время вышло");
+			break;
+		}
+		else
+		{
+			Sleep(1000);
+			sec--;
+			if (sec == 0)
+			{
+				min--;
+				sec = 59;
+				if (min == 0)
+				{
+					hours--;
+					min = 59;
+				}
+			}
+			if (sec != 0)
+			{
+				printf_s("%d:%d:%d\n", hours, min, sec);
+			}
+			else
+			{
+
+			}
+		}
+
+	}
+}
+int times(long int t)
+{
+	int sec = t;
+	int min = 0;
+	int hours = 0;
+	while (1)
+	{
+		while (sec > 60)
+		{
+				sec=sec - 60;
+				min++;
+		}
+		while (min > 60)
+		{
+			min = min - 60;
+			hours++;
+		}
+		while (hours > 24)
+		{			
+				hours = hours - 24;
+				if (hours < 24)
+				{
+					hours = hours + 3;
+				}
+		}
+		Sleep(1000);
+		sec++;
+		if (sec == 60)
+		{
+			min++;
+			sec = 0;
+			if (min == 60)
+			{
+				hours++;
+				min = 0;
+				if (hours == 24)
+				{
+					hours = 0;
+				}
+			}
+		}
+		printf_s("%d:%d:%d\n",hours,min,sec);
+	}
+}
+int main()
+{
+	system("chcp 1251>nul");
+	HANDLE hTread[3];
+	DWORD p=-1;
+	DWORD timer=0;
+	DWORD timer1 = 0;
+	DWORD timer2 = 0;
+	long int ttime;
+	while (1)
+	{
+		scanf_s("%d", &p);
+		switch (p)
+		{
+		case 1:
+			for (size_t i = 0; i < 2; i++)
+			{
+				SuspendThread(hTread[i]);
+			}
+			break;
+		case 2:
+			hTread[0] = CreateThread(NULL, 0, times, 0, NULL, NULL);
+			break;
+		case 3:		
+			SuspendThread(hTread[0]);
+			ttime= time(NULL);
+		  hTread[1] = CreateThread(NULL, 0, times, ttime, NULL, NULL);
+			break;
+		case 4:
+			scanf_s("%d", &timer);
+			scanf_s("%d", &timer1);
+			scanf_s("%d", &timer2);
+			while (timer != 0 || timer1 != 0)
+			{
+				if (timer != 0)
+				{
+					timer = timer - 1;
+					timer1 = timer1 + 60;
+				}
+				if (timer1 != 0)
+				{
+					timer1=timer1 - 1;
+					timer2 = timer2 + 60;
+				}
+			}
+			hTread[2] = CreateThread(NULL, 0, timers, timer2, NULL, NULL);
+			break;
+		case 0:
+
+			break;
+		default:
+			break;
+		}
+	}
+	WaitForSingleObject(hTread, INFINITE);
+}
